8f76b024515d1a0e5a16b52d6f0f7f3ae9f34716
JaxKluesner <jaxkluesner@gmail.com>
2020-03-12 00:13:25 -0500

    Added lambda functions for first derivatives (Might be poorly made)

--- SIR_Model.py
+++ SIR_Model.py
@@ -3,13 +3,16 @@ import matplotlib as mplib
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-s0 = int(input("Enter total susceptible population: "))
-i0 = int(input("Enter the number infected: "))
-r0 = int(input("Enter the number of recovered persons: "))
+susc = int(input("Enter initial susceptible population: "))
+inf = int(input("Enter the initial number infected: "))
+rec = int(input("Enter the initial number of recovered persons: "))
 inf_rate = float(input("Enter the infection rate: "))
 rec_rate = float(input("Enter the recovery rate: "))
-pop = s0 + i0 + r0
+pop = susc + inf + rec
 
 # Generally in notation as R0 but called threshold here.
-threshold = inf_rate * s0 / rec_rate
+threshold = inf_rate * susc / rec_rate
 
+s_prime = lambda susc: susc * inf_rate
+i_prime = lambda susc, inf: susc * inf_rate - inf * rec_rate
+r_prime = lambda inf: inf * rec_rate
