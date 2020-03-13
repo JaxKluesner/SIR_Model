2be7a2bcc03b6183d3a106e27eb7afd14c98bf0b
JaxKluesner <jaxkluesner@gmail.com>
2020-03-13 00:53:30 -0500

    Added graphing capabilities, DO NOT make R0 > 1. Need to be fixed.

--- SIR_Model.py
+++ SIR_Model.py
@@ -19,14 +19,28 @@ r_prime = lambda inf: inf * rec_rate
 
 #Timescale
 T = 100
-t = np.linspace(0,T,1000)
+t = np.linspace(0, T, 100)
 
 
 y0 = [susc, inf, rec]
-#print(y0)
 
+# The function of population over time f[susc, inf, rec].
+# First term is susceptible and so on separated by commas
 f = lambda y,t: [inf_rate * y[0] * y[1],
                  inf_rate * y[0] * y[1] - rec_rate * y[1],
                  rec_rate * y[1]]
 
+# Throws excess work warning? Calculates the solution to f over time
 y_sol = odeint(f, y0, t)
+
+# Beginning of graphing.
+fig = plt.figure(figsize=(15, 10));
+plt.plot(t, y_sol[:,0],'-', label= 'Susceptible')
+plt.plot(t, y_sol[:,1],'--', label= 'Infected')
+plt.plot(t, y_sol[:,2],'-.', label= 'Recovered')
+plt.ylim(0,pop)
+plt.xlim(0,100)
+plt.title("Population Over Time")
+plt.xlabel("Time (days)")
+plt.ylabel("Population")
+plt.legend()
